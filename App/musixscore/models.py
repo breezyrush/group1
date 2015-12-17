221cd424071a2ec5acab5550f9cf177ea505c0a4
catherinemaglasang <maglasangcatherine12@gmail.com>
2015-12-17 19:47:52 +0800

    added new field in class CD

--- App/musixscore/models.py
+++ App/musixscore/models.py
@@ -17,17 +17,16 @@ class Performer(models.Model):
 	def __unicode__(self):
 		return self.fname
 
-<<<<<<< HEAD
+
 class Song(models.Model):
 	title =  models.CharField(max_length = 50, null = True)
-=======
+
 
 class CD(models.Model):
 	title = models.CharField(max_length = 100, null = True)
 	performer = models.ForeignKey(Performer)
-	# songs = models.ForeignKey(Song)
+	songs = models.ForeignKey(Song)
 	
 	def __unicode__(self):
 		return self.title
 
->>>>>>> 65c4c32dbaa0506e092684ed8d8554c19eb77d9a
