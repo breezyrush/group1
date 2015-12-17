8a4f41bba6d343453c4e6cffa6c10c28fd9241cb
catherinemaglasang <maglasangcatherine12@gmail.com>
2015-12-17 19:20:48 +0800

    added class CD and modified admin.py

--- Problem/App/musixscore/models.py
+++ Problem/App/musixscore/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CD(models.Model):
+	title = models.CharField(max_length=300)
+	# songs = models.ForeignKey(Song)
+
+	def __unicode__(self):
+		return self.title
+
