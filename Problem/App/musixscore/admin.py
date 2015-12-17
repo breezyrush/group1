dda85a0a90a128ca8a607ddfa52c75169d28ebbe
erikkaBaguio <erikka.baguio@gmail.com>
2015-12-17 19:34:48 +0800

    Added SongAdmin in admin.py

--- Problem/App/musixscore/admin.py
+++ Problem/App/musixscore/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CD
+from .models import *
 
 
 class CDAdmin(admin.ModelAdmin):
 	pass		
 admin.site.register(CD, CDAdmin)
+
+class SongAdmin(admin.ModelAdmin):
+	pass
+admin.site.register(Song, SongAdmin)
