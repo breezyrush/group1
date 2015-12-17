2159297ac2036d085ac831ce1766b8ea33482e85
RoselleEbarle <roselle.ebarle04@gmail.com>
2015-12-17 19:19:50 +0800

    add views logic

--- App/musixscore/views.py
+++ App/musixscore/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def browse(request):
+	genre = request.GET.get('genre', '')
+	performer = request.GET.get('performer', '')
+	cd = request.GET.get('cd','')
+	
+
 	return render(request, 'browse/browse.html', {})
